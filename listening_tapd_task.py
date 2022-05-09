aa57bcfd4fc127968125b3ed7711cb892428c7c1
zt13415 <z13415@ztn.com>
2022-05-09 19:28:27 +0800

    优化tapd任务通知

--- listening_tapd_task.py
+++ listening_tapd_task.py
@@ -29,7 +29,7 @@ class ListeningTapdTask():
                 self._feishu_user_open_ids = x["user_open_ids"]
                 self._cookies = x["tapd_cookie"]
                 self._robot_url = x["feishu_robot_url"]
-                self._not_task_users_dict = {}
+                self._not_task_users_list = []
                 for project in x["tapd_projects"]:
                     self._project_id = project
                     resp = requests.get(f'https://www.tapd.cn/{self._project_id}/prong/tasks',
@@ -43,11 +43,12 @@ class ListeningTapdTask():
                             # print(card_msg)
                             self._send(card_msg)
 
-                        card_msg = self._get_not_task_message(task_list)
-                        if card_msg != None:
-                            # print(card_msg)
-                            self._send(card_msg)
                     print(f'执行项目Id：{self._project_id} 结束！')
+
+                card_msg = self._get_not_task_message()
+                if card_msg != None:
+                    # print(card_msg)
+                    self._send(card_msg)
 
     def _get_task_list(self, html_content):
         task_list = []
@@ -96,6 +97,21 @@ class ListeningTapdTask():
                                                         "header": {"template": "orange",
                                                                    "title": {"content": f"🏷️ {self._project_name}",
                                                                              "tag": "plain_text"}}}}
+        # 查找当天任务 9,10
+        if self.now_datetime.hour >= 9 and self.now_datetime.hour <= 10:
+            for u in self._feishu_user_open_ids:
+                is_exists = False
+                for x in task_list:
+                    to_date_time = datetime.datetime.strptime(x["to_time"], "%Y-%m-%d")
+                    if u == x["user"] and to_date_time >= self.expires_time:
+                        self._not_task_users_list.remove(u)
+                        is_exists = True
+                        break
+
+                if is_exists == False:
+                    if u not in self._not_task_users_list:
+                        self._not_task_users_list.append(u)
+
         # 正常任务提醒 设置时段 18,19,20
         for x in task_list:
             to_date_time = datetime.datetime.strptime(x["to_time"], "%Y-%m-%d")
@@ -122,14 +138,14 @@ class ListeningTapdTask():
 
     def _get_not_task_users_template(self):
         at_users = ""
-        for u in self._not_task_users_dict:
-            at_users += f"<at id={self._not_task_users_dict[u]}></at>"
+        for u in self._not_task_users_list:
+            at_users += f"<at id={self._feishu_user_open_ids[u]}></at>"
         return {"extra": {"tag": "button", "text": {"content": "👉 去创建", "tag": "plain_text"}, "type": "danger",
                           "url": "https://www.tapd.cn/company/participant_projects"}, "tag": "div",
                 "text": {"content": f"发现你今日任务未创建。{at_users}",
                          "tag": "lark_md"}}
 
-    def _get_not_task_message(self, task_list):
+    def _get_not_task_message(self):
         _init_msg = {"msg_type": "interactive", "card": {"config": {"enable_forward": True}, "elements": [],
                                                          "header": {"template": "red",
                                                                     "title": {"content": f"👻️ 我的任务",
@@ -138,19 +154,7 @@ class ListeningTapdTask():
                                                         "header": {"template": "red",
                                                                    "title": {"content": f"👻️ 我的任务",
                                                                              "tag": "plain_text"}}}}
-        # 查找当天任务 9,10
-        if self.now_datetime.hour >= 9 and self.now_datetime.hour <= 10:
-            for u in self._feishu_user_open_ids:
-                is_exists = False
-                for x in task_list:
-                    to_date_time = datetime.datetime.strptime(x["to_time"], "%Y-%m-%d")
-                    if u == x["user"] and to_date_time >= self.expires_time:
-                        is_exists = True
-                        break
-
-                if is_exists == False:
-                    self._not_task_users_dict.update({u: self._feishu_user_open_ids[u]})
-        if len(self._not_task_users_dict) != 0:
+        if len(self._not_task_users_list) != 0:
             card_msg["card"]["elements"].append(self._get_not_task_users_template())
         if _init_msg == card_msg:
             return None
